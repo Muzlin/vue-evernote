83cc26077042e1542bb86d9d496095c028e2717e
MuzLin <1030996014@qq.com>
2018-05-14 22:58:14 +0800

    完成markdown预览

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -4,21 +4,21 @@
     <NoteSidebar @watch:notes="val => notes = val" />
     <div class="note-detail">
       <div class="note-empty" v-show="!curNote.id">请选择笔记</div>
-      <div v-show="curNote.id">
+      <div  class="note-content" v-show="curNote.id">
         <div class="note-bar">
           <span> 创建日期：{{curNote.createdAtFriendly}}</span>
           <span> 更新日期：{{curNote.updatedAtFriendly}}</span>
           <span>{{statusText}}</span>
           <span @click="deleteNote" class='iconfont icon-trash'></span>
-          <span class='iconfont icon-quanping'></span>
+          <span @click="markdown" :class="`iconfont ${isShowPreview?'icon-tuichuquanping':'icon-quanping'}`"></span>
         </div>
         <div class="note-title">
           <input type="text" v-model="curNote.title" @input="updateNote" @keydown="statusText='正在输入...'" placeholder="输入标题">
 
         </div>
         <div class="editor">
-          <textarea v-model="curNote.content" @input="updateNote" @keydown="statusText='正在输入...'" placeholder="输入内容 支持markdown"></textarea>
-          <div class="preview markdown-body" v-show="false"></div>
+          <textarea v-show="!isShowPreview" v-model="curNote.content" @input="updateNote" @keydown="statusText='正在输入...'" placeholder="输入内容 支持markdown"></textarea>
+          <div class="preview markdown-body" v-html="previewContent" v-show="isShowPreview"></div>
         </div>
       </div>
     </div>
@@ -32,6 +32,9 @@
   import Bus from '@/helpers/bus'
   import _ from 'lodash' // 截流模块
   import Notes from '@/apis/notes'
+  import MarkdownIt from 'markdown-it'
+
+  let md = new MarkdownIt()
 
   export default {
     name: 'NoteDetail',
@@ -42,7 +45,8 @@
       return {
         curNote: {},
         notes: [],
-        statusText:'未改动'
+        statusText:'未改动',
+        isShowPreview: false
       }
     },
     created() {
@@ -68,6 +72,11 @@
       this.curNote = this.notes.find(note => note.id == to.query.noteId) || {}
       next()
     },
+    computed:{
+      previewContent(){
+        return md.render(this.curNote.content+'')
+      }
+    },
     methods:{
       // 用户输入的时候自动保存
       // 这里使用截流函数 保持一定的事件内调用更新接口
@@ -88,6 +97,9 @@
           this.notes.splice(this.notes.indexOf(this.curNote),1)
           this.$router.replace({path:'/note'})
         })
+      },
+      markdown(){
+        this.isShowPreview = !this.isShowPreview
       }
     }
   }
